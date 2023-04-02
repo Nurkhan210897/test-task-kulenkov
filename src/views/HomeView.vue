15d0af3dd3659f395aea51ddc5602dd5920ae7c8
Нурхан Дилдабек <n.dildabek@mycar.kz>
2023-04-02 19:50:00 +0600

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <baseCard></baseCard>
+  </div>
+</template>
+
+<script>
+import BaseButton from '../components/base/BaseButton.vue'
+import BaseCard from '../components/base/BaseCard.vue'
+export default {
+  components: { BaseButton, BaseCard }
+}
+</script>
+
+<style></style>
